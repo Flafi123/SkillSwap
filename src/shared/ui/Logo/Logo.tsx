7b8fc474d576a1919bc12ee7870abe7ea61e70b3
Flafi123 <ubiwaka1@bk.ru>
2026-03-29 19:04:04 +0300

    refactor: change Logo div to Link

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,11 +1,12 @@
 import styles from './Logo.module.css'
 import logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
 
 export const Logo = () => {
   return (
-    <div className={styles.logoContainer}>
+    <Link to="/" className={styles.logoContainer}>
       <img src={logo} alt="Logo" className={styles.logoImage} />
       <span className={styles.text}>SkillSwap</span>
-    </div>
+    </Link>
   )
 }

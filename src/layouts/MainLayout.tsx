723c159e0398c079b78bde44bbfb7975a7d23d36
NEONIX <timuraridi@yandex.ru>
2026-03-30 15:39:20 +0300

    fix: исправления по линтеру + hover и focus

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -29,7 +29,7 @@ export const MainLayout: React.FC = () => {
       <main>
         <Outlet />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,6 +1,6 @@
-import { Logo } from '../../shared/ui/Logo';
-import { Link } from "react-router-dom";
-import styles from './Footer.module.css';
+import { Logo } from '../../shared/ui/Logo'
+import { Link } from 'react-router-dom'
+import styles from './Footer.module.css'
 
 export const Footer = () => {
   return (
@@ -8,32 +8,56 @@ export const Footer = () => {
       <div className={styles.container}>
         <div className={styles.colLeft}>
           <Logo />
-          <p className={styles.copyright}>SkillSwap — 2025</p>
+          <p className={styles.copyright}>SkillSwap — 2025</p>
         </div>
 
         <div className={styles.rightSection}>
           <nav className={styles.col}>
-            <ul className={`${styles.links} ${styles.circle}`}>
-              <li><Link to="" className={styles.link}>О проекте</Link></li>
-              <li><Link to="" className={styles.link}>Все навыки</Link></li>
+            <ul className={styles.links}>
+              <li>
+                <Link to="" className={styles.link}>
+                  О проекте
+                </Link>
+              </li>
+              <li>
+                <Link to="" className={styles.link}>
+                  Все навыки
+                </Link>
+              </li>
             </ul>
           </nav>
 
           <nav className={styles.col}>
             <ul className={styles.links}>
-              <li><Link to="" className={styles.link}>Контакты</Link></li>
-              <li><Link to="" className={styles.link}>Блог</Link></li>
+              <li>
+                <Link to="" className={styles.link}>
+                  Контакты
+                </Link>
+              </li>
+              <li>
+                <Link to="" className={styles.link}>
+                  Блог
+                </Link>
+              </li>
             </ul>
           </nav>
 
           <nav className={styles.col}>
             <ul className={styles.links}>
-              <li><Link to="" className={styles.link}>Политика конфиденциальности</Link></li>
-              <li><Link to="" className={styles.link}>Пользовательское соглашение</Link></li>
+              <li>
+                <Link to="" className={styles.link}>
+                  Политика конфиденциальности
+                </Link>
+              </li>
+              <li>
+                <Link to="" className={styles.link}>
+                  Пользовательское соглашение
+                </Link>
+              </li>
             </ul>
           </nav>
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}

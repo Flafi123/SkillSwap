3c60936ab279d0df3264210a589a8ae9453fa01b
lensoleilleee <luiza.atchemyan@gmail.com>
2026-04-01 22:32:55 +0400

    Вёрстка страницы 500 (#39)
    * верстка error 404
    
    * верстка error 404
    
    * fix lint error 404
    
    * Вёрстка страницы 500
    
    * внесение исправлений по замечаниям в css

---
+++ src/pages/Error/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './NotFoundPage.module.css'
+import { Button } from '../../../shared/ui/Button/Button'
+import { Link } from 'react-router-dom'
+
+import errorImage_404 from '../../../shared/assets/svg/error_404.svg'
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img src={errorImage_404} alt="Ошибка 404" className={styles.image} />
+
+        <h1 className={styles.title}>Страница не найдена</h1>
+
+        <p className={styles.description}>
+          К сожалению, эта страница недоступна. Вернитесь <br></br> на главную страницу или
+          попробуйте позже
+        </p>
+
+        <div className={styles.actions}>
+          <Button variant="secondary">Сообщить об ошибке</Button>
+
+          <Link to="/">
+            <Button>На главную</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Error/ServerErrorPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const ServerErrorPage: React.FC = () => {
-  return <div>Ошибка на сервере.</div>
-}

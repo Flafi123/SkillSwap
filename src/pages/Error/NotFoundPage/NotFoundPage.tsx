0b85b41b9c92c43578db149da7628b3a293933d8
Luiza Adzhemian <luiza.atchemyan@gmail.com>
2026-04-04 17:06:17 +0400

    Вынесла стили страниц ошибок в один файл

--- src/pages/Error/NotFoundPage/NotFoundPage.tsx
+++ src/pages/Error/NotFoundPage/NotFoundPage.tsx
@@ -2,47 +2,30 @@ import React, { useState } from 'react'
 import styles from '../ErrorPage.module.css'
 import { Button } from '../../../shared/ui/Button/Button'
 import { Link } from 'react-router-dom'
-import { Modal } from '../../../shared/ui/Modal'
 
 import errorImage_404 from '../../../shared/assets/svg/error_404.svg'
 
 export const NotFoundPage: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <img src={errorImage_404} alt="Ошибка 404" className={styles.image} />
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img src={errorImage_404} alt="Ошибка 404" className={styles.image} />
 
-          <h1 className={styles.title}>Страница не найдена</h1>
+        <h1 className={styles.title}>Страница не найдена</h1>
 
-          <p className={styles.description}>
-            К сожалению, эта страница недоступна. Вернитесь <br />
-            на главную страницу или попробуйте позже
-          </p>
+        <p className={styles.description}>
+          К сожалению, эта страница недоступна. Вернитесь <br></br> на главную страницу или
+          попробуйте позже
+        </p>
 
-          <div className={styles.actions}>
-            <Button variant="secondary" onClick={() => setIsModalOpen(true)}>
-              Сообщить об ошибке
-            </Button>
+        <div className={styles.actions}>
+          <Button variant="secondary">Сообщить об ошибке</Button>
 
-            <Link to="/">
-              <Button>На главную</Button>
-            </Link>
-          </div>
+          <Link to="/">
+            <Button>На главную</Button>
+          </Link>
         </div>
       </div>
-
-      {/* МОДАЛКА */}
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <div style={{ textAlign: 'center' }}>
-          <h2>Сообщение отправлено</h2>
-          <p>Спасибо! Мы уже разбираемся с проблемой.</p>
-
-          <Button onClick={() => setIsModalOpen(false)}>Закрыть</Button>
-        </div>
-      </Modal>
-    </>
+    </div>
   )
 }

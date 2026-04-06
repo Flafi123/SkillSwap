97b21a713bf02678d5e6153d8011082122f36365
Flafi123 <ubiwaka1@bk.ru>
2026-04-06 16:35:02 +0300

    feat: fetch initial data on app mount

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { appRouter } from './appRouter'
 import './index.css'
 import '../shared/assets/fonts/fonts.css'
+import { getAllSkills, getAllCategories, getAllSubcategories } from '../entities/Skill/model/skillSlice'
+import { getAllUsers } from '../entities/user/model/userSlice'
+import { useAppDispatch } from './store/store'
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getAllSkills())
+    dispatch(getAllCategories())
+    dispatch(getAllSubcategories())
+    dispatch(getAllUsers())
+  }, [dispatch])
+
   return <RouterProvider router={appRouter} />
 }
 

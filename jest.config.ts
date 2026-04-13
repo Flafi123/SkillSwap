fc1ff772fec96dfb5540d803a8e6c6fae5d1b2bf
Flafi123 <ubiwaka1@bk.ru>
2026-04-13 20:56:49 +0300

    Добавление конфига jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+
+//   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+
+  clearMocks: true,
+};
+
+export default config;
